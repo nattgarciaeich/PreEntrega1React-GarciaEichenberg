22eaf4df45b267c3e1c135874c193b546814864c
NattGarciaEich <nattgarciaeich@gmail.com>
2023-09-11 16:50:32 -0300

    incorporacion de los componentes para la entrega

---
+++ src/componentes/NavBar/CardWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BiCart} from 'react-icons/bi'
+
+const CardWidget = () => {
+    return(
+        <div>
+            <BiCart/> <span>0</span>
+        </div>
+
+    )
+}
+
+export default CardWidget
